9341deda290f364bb53bad1e718d4616b13979d9
Qin Jian <love.infly@gmail.com>
2013-02-27 20:00:39 +0800

    geektools

---
+++ python/geektool倒计时.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#!/bin/python
+
+
+import datetime
+
+
+t2 = datetime.date(2013,06,23)
+t1 = datetime.date.today()
+
+delta_days = (t2 - t1).days
+
+print "到截止时间还有" ,delta_days, "天"
