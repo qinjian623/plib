b8ed9c7f2e08fab601ace0211018ef2493b76bec
Qin Jian <qinjian623@gmail.com>
2014-10-29 17:39:31 +0800

    label tools

--- opencv/opencv.cpp
+++ opencv/opencv.cpp
@@ -17,47 +17,84 @@ class SymmetryDetector
 {
 	public:
 	SymmetryDetector(Mat mat){
-		for(int i = 0; i < mat.cols; i++){
-			int sum = 0;
-			for(int j = 0; j < mat.rows; j++){
-				sum += g(mat, j, i);
-			}
-			h[i] = sum;
+		if (!mat.isContinuous()){
+			// TODO error?
 		}
+		axis.resize(mat.cols);
+
+		for(int x = 0; x < mat.cols; x++){
+			int sum = 0;
+			for(int y = 0; y < mat.rows; y++){
+				sum += abs((f(mat, x+1, y-1) + 2*f(mat, x+1 , y ) + f(mat, x+1, y+1))
+					   - (f(mat, x-1, y-1) + 2*f(mat, x-1, y) + f(mat, x-1 , y+1)));
+			}
+			axis[x] = sum;
+		}
+		width = mat.cols;
+		center = width/2;
+		float e_function_sum = 0.0;
+		for(int x=0; x < width; ++x){
+			e_function_sum += E(x-center, center);
+		}
+		e_function_avg = e_function_sum / width;
+	}
+
+	bool is_symmetry(){
+		float s_c = S(center);
+		//cout << s_c<< endl;
+		return s_c > 0.0? true : false;
 	}
 
 	private :
-	vector<int> h;
-	uchar get(Mat mat, int x, int y){
+	vector<int> axis;
+	float e_function_avg;
+	int width;
+	int center;
+
+	///////////////////////
+        // 初始化获得投影
+        ///////////////////////
+	uchar get(Mat &mat, int x, int y){
 		return mat.data[y*mat.cols+x];
 	}
-	float avg;
-	uchar f(Mat mat, int x, int y){
+
+	uchar f(Mat &mat, int x, int y){
 		return get(mat, x, y);
 	}
 
-	int g(Mat mat, int x, int y){
-		return abs((f(mat, x+1, y-1) + 2*f(mat, x+1 , y ) + f(mat, x+1, y+1))
-			   - (f(mat, x-1, y-1) + 2*f(mat, x-1, y) + f(mat, x-1 , y+1)));
-	}
-
-	int G(vector<int> axis, int x){
+	////////////////
+        // 对称性分析
+        ////////////////
+	int g(int x){
 		return axis[x];
 	}
 
-	float O(vector<int> axis, int u, int x_s){
-		return (G(axis, x_s+u) - G(axis, x_s-u))/2;
+	float O(int u, int x_s){
+		return (g(x_s+u) - g(x_s-u))/2;
 	}
 
-	float E(vector<int> axis, int u, int x_s){
-		return (G(axis, x_s+u) + G(axis, x_s-u))/2;
+	float E(int u, int x_s){
+		return (g(x_s+u) + g(x_s-u))/2;
 	}
 
-	float E_n(){
+	float E_n(int u, int x_s){
+		return E(u, x_s) - e_function_avg;
 	}
 
 	int u(int x, int x_s){
 		return x - x_s;
+	}
+
+	float S(int x_s){
+		float en_sum_of_squares = 0.0;
+		float o_sum_of_squares = 0.0;
+		for(int x = 0; x < width; ++x){
+			float en_val = E_n(x-x_s, x_s);
+			en_sum_of_squares += en_val*en_val;
+			float o_val = O(x-x_s, x_s);
+			o_sum_of_squares += o_val*o_val;
+		}
+		return (en_sum_of_squares - o_sum_of_squares)/(en_sum_of_squares + o_sum_of_squares);
 	}
 };
 
@@ -77,7 +114,7 @@ void init()
 	blob_detector= new SimpleBlobDetector(params);
 	blob_detector->create("SimpleBlob");
 
-	canny_thread = 40;
+	canny_thread = 30;
 	pair_thread = 5;
 }
 
@@ -153,39 +190,39 @@ void car_rear_dection_ROI(Mat &image0)
 	Mat hsv, gray, mask1, mask0, mask, blured, sobel;
 
 	// 去除左上和右上角
-	if (image0.isContinuous()){
-		for (int x = 0; x < image0.cols/3; x++){
-			for (int y = 0; y< image0.rows/3;y++){
-				int i = x+y*image0.cols;
-				d[i*3] = 255;
-				d[i*3+1] = 255;
-				d[i*3+2] = 255;
-			}
-		}
-		for (int x = image0.cols/3*2; x < image0.cols; x++){
-			for (int y = 0; y< image0.rows/3;y++){
-				int i = x+y*image0.cols;
-				d[i*3] = 255;
-				d[i*3+1] = 255;
-				d[i*3+2] = 255;
-			}
-		}
-	}
+	// if (image0.isContinuous()){
+	// 	for (int x = 0; x < image0.cols/3; x++){
+	// 		for (int y = 0; y< image0.rows/3;y++){
+	// 			int i = x+y*image0.cols;
+	// 			d[i*3] = 255;
+	// 			d[i*3+1] = 255;
+	// 			d[i*3+2] = 255;
+	// 		}
+	// 	}
+	// 	for (int x = image0.cols/3*2; x < image0.cols; x++){
+	// 		for (int y = 0; y< image0.rows/3;y++){
+	// 			int i = x+y*image0.cols;
+	// 			d[i*3] = 255;
+	// 			d[i*3+1] = 255;
+	// 			d[i*3+2] = 255;
+	// 		}
+	// 	}
+	// }
 	//blur(hsv, hsv, Size(3, 3));
 	// 灰度空间
 	cvtColor(image0, gray, CV_BGR2GRAY);
 	Sobel(gray, sobel, CV_8U, 1, 0);
-	imshow("sobel", sobel);
+	//imshow("sobel", sobel);
 	// 边界检测，以准备下一步的hough变换
-	M = CannyThreshold(sobel);
+	M = CannyThreshold(gray);
 
 	// 模糊以减少噪音
 	// hsv空间，以更好的过滤红色
 	blur(image0, blured, Size(5,5));
 	cvtColor(blured, hsv, CV_BGR2HSV);
 	// 获得过滤红色的mask
-	inRange(hsv, Scalar(165, 60, 0),Scalar(180, 255, 255), mask1);
-	inRange(hsv, Scalar(0, 60, 0),Scalar(5, 255, 255), mask0);
+	inRange(hsv, Scalar(165, 40, 0),Scalar(180, 255, 255), mask1);
+	inRange(hsv, Scalar(0, 40, 0),Scalar(5, 255, 255), mask0);
 	cv::bitwise_or(mask0, mask1, mask);
 
 	vector<Vec2f> all_lines;
@@ -203,22 +240,19 @@ void car_rear_dection_ROI(Mat &image0)
 	// 		}
 	// 	}
 	// }
-
-
 	vector<KeyPoint> keypoints;
 	blob_detector->detect(mask, keypoints);
 	//blob_detector->detect(gray, keypoints);
 
 	vector< pair<KeyPoint, KeyPoint> > pairs;
 	find_pairs(pairs, keypoints);
-
 	for (size_t i = 0; i < pairs.size(); i++) {
 		KeyPoint first = pairs[i].first;
 		KeyPoint second = pairs[i].second;
 		//cout << first.pt << "," << second.pt<< endl;
-		float rect_height = abs(first.pt.x - second.pt.x)/3;
+		float rect_height = abs(first.pt.x - second.pt.x)/2;
 		float rect_min_x = min(first.pt.x, second.pt.x);
-		float rect_min_y = min(first.pt.y, second.pt.y);
+		float rect_min_y = min(first.pt.y, second.pt.y) - 10;
 		if (rect_min_y < image0.rows*0.33){
 		 	continue;
 		}
@@ -230,7 +264,7 @@ void car_rear_dection_ROI(Mat &image0)
 		float distance_thredhold = calc_distance_thread(rect_min_y, row, col);// - (3.0/7.0*col);
 		// 200? (rect_min_y/image0.rows * 200?
 		//if (distance> 100 || distance < 20){
-		if (distance> 800 || distance < 20){
+		if (distance > 100 || distance < 20){
 			continue;
 		}
 		// TODO 调整参数的问题
@@ -248,21 +282,28 @@ void car_rear_dection_ROI(Mat &image0)
 		//imshow("small gray", smallImage);
 		smallImage = Mat(M, ROI);
 		vector<Vec2f> lines;
-		HoughLines(M, lines, 1, CV_PI/180, 50, 0, 0 );
+		HoughLines(smallImage, lines, 1, CV_PI/180, smallImage.cols/2, 0, 0 );
 		bool isRear = false;
+
 		for( size_t i = 0; i < lines.size(); i++ ){
 			float theta = lines[i][1];
 			//TODO 横线检测，0度的问题
-			if (!(theta > 3.14/2-0.01 && theta < 3.14/2 + 0.01)){
-				continue;
+			if (theta > 3.14/2-0.01 && theta < 3.14/2 + 0.01){
+				isRear = true;
+				break;
 			}
-			isRear = true;
-			break;
+			if (theta > 0-0.01 && theta < 0 + 0.01){
+				isRear = true;
+				break;
+			}
 		}
+		//smallImage = Mat(, ROI);
+		//SymmetryDetector sd(smallImage);
+		//&&sd.is_symmetry() (!sd.is_symmetry())
 		if (isRear){
 			rectangle(image0, ROI, Scalar(0, 0, 255),2);
-		}else{
-			//rectangle(image0, ROI, Scalar(0, 0, 255));
+		}else if (isRear){
+			//rectangle(image0, ROI, Scalar(0, 255, 0));
 		}
 	}
 	for( size_t i = 0; i < all_lines.size(); i++ ){
@@ -284,9 +325,9 @@ void car_rear_dection_ROI(Mat &image0)
 		Point pt1(0,i),pt2(d,i);
 		//line( image0, pt1, pt2, Scalar(0,255,0), 1, CV_AA);
 	}
-	imshow("Mask", mask);
-	imshow("Canny",M);
-	drawKeypoints(image0, keypoints, image0);
+	//imshow("Mask", mask);
+	//imshow("Canny",M);
+	//drawKeypoints(image0, keypoints, image0);
 }
 
 
@@ -299,18 +340,26 @@ int main(int argc, char *argv[])
 	VideoCapture video;
 	namedWindow("output", 1);
 	init();
+	waitKey(0);
+	bool done = false;
 
 	if(!video.open(videoFile))
 		return 1;
 	for(;;){
 		video>>src;
+		//src = imread(argv[1], 1);
 		//cout << src.rows << ',' << src.cols<< endl;
 		if(src.empty())
 			break;
 		pyrDown(src, src, Size(src.cols/2, src.rows/2));
 		car_rear_dection_ROI(src);
 		imshow("output",src);
-		waitKey(1);
+		if (done){
+			waitKey(1);
+		}else{
+			done = true;
+			waitKey(0);
+		}
 		//exit(0);
 	}
 	return 0;
