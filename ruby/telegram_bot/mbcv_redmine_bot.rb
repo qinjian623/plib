eacb25b6a0ee4e5df3d7b0cf67b03398a76b2c90
Qin Jian <qinjian623@gmail.com>
2016-01-05 15:06:33 +0800

    reminder bot

--- ruby/telegram_bot/mbcv_redmine_bot.rb
+++ ruby/telegram_bot/mbcv_redmine_bot.rb
@@ -8,8 +8,16 @@ def new_user (username, mail, password)
   @contex[username][:password] = password
 end
 
+def command_start(message)
+  puts JSON.pretty_generate(message)
+  username = message['from']['id']
+  @contex[username] = {}
+  @contex[username][:latest_chat_it] = message['chat']['id']
+  return "User storage space created."
+end
+
 def command_register(message)
-  username = message['from']['username']
+  username = message['from']['id']
   mo = /\/(\w*) *(\w*@\w*\.com)\s*(\w*)/.match(message['text'])
   if mo
     mail = mo[2]
@@ -31,7 +39,29 @@ def command_register(message)
 end
 
 def command_reminder(message)
-  
+  segs = message['text'].split(/\s/)
+  t = Time.now
+  msg = "Alarm"
+  if segs.length == 3
+    msg = segs[2]
+    t =  t + segs[1].to_i * 60
+  elsif segs.length == 2
+    t = t + segs[1].to_i * 60
+  else
+    return "Wrong command format"
+  end
+  q = {
+    :chat_id => message['chat']['id'],
+    :text => msg,
+    :message_id => message['message_id'],
+    :t => t
+  }
+  puts JSON.pretty_generate(q)
+  @contex[:Q].push(q)
+  @contex[:Q].sort!{ |left,  right|
+    left[:t] <=> right[:t]
+  }
+  return "Will reminder you at " + t.to_s
 end
 
 @key = ARGV[0]
@@ -86,17 +116,50 @@ def handle_update(update)
 end
 
 @contex = {
-  
+  :Q => [],
+  :last_update_id => -1
 }
 
-updates = call(:getUpdates)
-puts JSON.pretty_generate(updates.last)
-updates.each do | update|
-  handle_update(update)
+
+def process_q()
+  now = Time.now
+  idx = 0
+  for q in @contex[:Q]
+    if q[:t] < now
+      idx = idx + 1
+      call(:sendMessage,
+           {'chat_id' => q[:chat_id],
+            'text' => q[:text],
+            'reply_to_message_id' => q[:message_id]})
+    end
+  end
+  @contex[:Q].shift(idx)
 end
 
-@contex[:last_update_id] = updates.last['update_id']
 
-puts @contex[:last_update_id]
+debug = true
+
+while true
+  if File.exists? 'contex.db'
+    puts "Reading db..."
+    @contex = Marshal.load(File.read('contex.db'))
+    puts @contex
+  end
+  process_q
+  updates = call(:getUpdates,
+                 {'offset' => @contex[:last_update_id] + 1})
+  # puts JSON.pretty_generate(updates.last)
+  updates.each do | update|
+    handle_update(update)
+  end
+  if updates.last
+    @contex[:last_update_id] = updates.last['update_id']
+  end
+  File.write('contex.db', Marshal.dump(@contex))
+  puts @contex if debug
+  break
+end
+
+
 # puts Marshal.dump(ret)
 
