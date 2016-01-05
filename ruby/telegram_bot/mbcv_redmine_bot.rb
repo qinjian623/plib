7c94ab19a245b00c3e1a01e55f662f580dfb1a54
Qin Jian <qinjian623@gmail.com>
2016-01-05 17:05:32 +0800

    list issues

--- ruby/telegram_bot/mbcv_redmine_bot.rb
+++ ruby/telegram_bot/mbcv_redmine_bot.rb
@@ -1,6 +1,20 @@
 require 'httpclient'
 require 'json'
 require 'digest'
+require 'erb'
+@redmine_domain = "http://192.168.1.244:3000"
+@redmine_methods = {
+  :issues => "http://192.168.1.244:3000/issues.json",
+}
+
+@output_template = "# <%= issue['id'] %> *<%= issue['subject'] %>* <% if not issue['description'].empty? %> _<%= issue['description']%>_ <% end %>"
+
+def call_redmine(method, username, password, paras = {})
+  client = HTTPClient.new
+  client.set_auth(@redmine_domain, username, password)
+  JSON.parse(client.get(@redmine_methods[:issues], paras).content)
+end
+
 
 def new_user (username, mail, password)
   @contex[username] = {}
@@ -16,23 +30,25 @@ def command_start(message)
   return "User storage space created."
 end
 
+def command_issues(message)
+  id = message['from']['id']
+  rm_username = @contex[id][:mail]
+  rm_passowrd = @contex[id][:password]
+  json = call_redmine(:issues, rm_username, rm_passowrd, {'assigned_to_id' => 'me'})
+  return (json['issues'].map do |issue|
+    render = ERB.new(@output_template)
+    render.result(binding)
+  end).join("\n")
+end
+
 def command_register(message)
   username = message['from']['id']
-  mo = /\/(\w*) *(\w*@\w*\.com)\s*(\w*)/.match(message['text'])
+  mo = message['text'].split(/\s/)
   if mo
-    mail = mo[2]
-    password = Digest::SHA256.digest mo[3]
-    if not @contex.key? username
-      new_user username, mail, password
-      return "New user added."
-    else
-      if  @contex[username][:password] == password
-        @contex[username][:mail] = mail
-        return "User mail reset done."
-      else
-        return "Wrong password."
-      end
-    end
+    mail = mo[1]
+    password = mo[2]
+    new_user username, mail, password
+    return "User registered."
   else
     return "Wrong command format."
   end
@@ -92,7 +108,8 @@ def dispatch(command, message)
     call(:sendMessage,
          {'chat_id' => message['chat']['id'],
           'text' => response,
-          'reply_to_message_id' => message['message_id']})
+          'reply_to_message_id' => message['message_id'],
+          'parse_mode' => 'Markdown'})
   rescue NoMethodError
     puts "Command: " + command + " not found."
   end
@@ -143,12 +160,11 @@ while true
   if File.exists? 'contex.db'
     puts "Reading db..."
     @contex = Marshal.load(File.read('contex.db'))
-    puts @contex
+    # puts @contex if debug
   end
   process_q
   updates = call(:getUpdates,
                  {'offset' => @contex[:last_update_id] + 1})
-  # puts JSON.pretty_generate(updates.last)
   updates.each do | update|
     handle_update(update)
   end
@@ -156,8 +172,8 @@ while true
     @contex[:last_update_id] = updates.last['update_id']
   end
   File.write('contex.db', Marshal.dump(@contex))
-  puts @contex if debug
-  break
+  # puts @contex if debug
+  sleep(10)
 end
 
 
